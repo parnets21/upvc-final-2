9dca2e0fdc1b60579a8cf1462a7fec73ea69e785
jonathan-armal <kirankumar47230@gmail.com>
2026-04-09 10:58:47 +0530

    push before escrow payment gateway

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const User = require('../models/Buyer/User');
 exports.authenticate = async (req, res, next) => {
   try {
     // Get token from header
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split  (' ')[1];
     
     if (!token) {
       return res.status(401).json({
